f221ad730c0aa7252e9edb7d3045389f825b8241
Xander-C <1019104544@qq.com>
2021-05-29 13:46:30 +0800

    fix: 修复刚进入时垃圾类型显示不正确

--- src/main/resources/static/js/demo/datatables-demo.js
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -1,6 +1,7 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#dataTable').DataTable();
+  updateTypes();
 });
 
 document.querySelectorAll(".bin-delete-btn").forEach((i)=>{i.addEventListener("click", async ()=>{
@@ -28,7 +29,9 @@ document.querySelectorAll(".bin-edit-btn").forEach((i)=>{i.addEventListener("cli
 
 const typeEnum = {"RECYCLABLE":"可回收垃圾", "WASTE":"厨余垃圾", "DRY":"干垃圾", "WET":"湿垃圾", "HARMFUL":"有害垃圾", "OTHER":"其他垃圾"};
 
-$("#location-select").on("change", ()=>{
+$("#location-select").on("change", ()=>{updateTypes();})
+
+const updateTypes = ()=>{
   const v = $("#location-select option:selected").val();
   document.getElementById("type-select").options.length = 0;
   getTypes(v).then((types)=>{
@@ -40,8 +43,8 @@ $("#location-select").on("change", ()=>{
     const typeSelect = $("#type-select")
     console.log(optionList);
     typeSelect.append(optionList);
-  })
-})
+  });
+}
 
 const getTypes = async (v) =>{
   const response = await fetch(`/getTypes?location=${v}`);
